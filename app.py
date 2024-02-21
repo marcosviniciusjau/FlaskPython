7313e3090ac2d323c61b8e6872a8b6061d0998c8
marcosviniciusjau <marcosviniciusjahu@gmail.com>
2024-02-21 16:52:18 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, jsonify,request
+from models.task import Task
+
+app= Flask(__name__)
+
+tasks= []
+task_id_control= 1
+
+@app.route("/tasks", methods=['POST', 'GET'])
+def create_task():
+    global task_id_control
+    data= request.get_json()
+    new_task= Task(id=task_id_control,title=data["title"],description=data.get("description",""))
+    task_id_control +=1
+    tasks.append(new_task)
+    print(tasks)
+    return jsonify({"message":"Nova tarefa criada com sucesso"})
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    task_list= []
+    for task in tasks:
+        task_list.append(task.to_dict())
+        
+    output={
+        "tasks":task_list,
+        "total_tasks":0
+    }
+
+    return jsonify(output)
+
+if __name__ == "__main__":
+    app.run(debug= True)
+    
