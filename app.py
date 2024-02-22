57bdf94fe8a245859f1f07708d982acbd86af7f2
marcosviniciusjau <marcosviniciusjahu@gmail.com>
2024-02-22 15:03:55 -0300

    update of the CRUD

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app= Flask(__name__)
 tasks= []
 task_id_control= 1
 
-@app.route("/tasks", methods=['POST', 'GET'])
+@app.route("/tasks", methods=['POST'])
 def create_task():
     global task_id_control
     data= request.get_json()
@@ -18,7 +18,7 @@ def create_task():
 
 @app.route('/tasks', methods=['GET'])
 def get_tasks():
-    task_list= [task.todict() for task in tasks]
+    task_list= [task.to_dict() for task in tasks]
        
     output = {
     "tasks": task_list,
@@ -32,6 +32,22 @@ def get_task(id):
         if t.id == id:
             return jsonify(t.to_dict())
         return jsonify({"message": "Não foi possivel encontrar a atividade"}),404
+
+@app.route('/tasks/<int:id>', methods=['PUT'])
+def update_task(id):
+    
+    task = None
+    for t in tasks:
+        if t.id == id:
+            task=t
+    if task == None:
+        return jsonify({"message":"Não foi possivel encontrar a atividade"}),404
+    data= request.get_json()
+    task.title = data['title']
+    task.description = data['description']
+    task.completed = data['completed']
+    print(task)
+    return jsonify({"message": "Tarefa atualizada com sucesso"})
 
 if __name__ == "__main__":
     app.run(debug= True)
